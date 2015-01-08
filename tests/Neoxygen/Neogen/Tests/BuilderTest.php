8f940837b2e85c1bb3cfe3a193fc8fa76bc8607a
Christophe Willemsen <willemsen.christophe@gmail.com>
2015-01-08 13:36:08 +0100

    added some tests for build time

--- tests/Neoxygen/Neogen/Tests/BuilderTest.php
+++ tests/Neoxygen/Neogen/Tests/BuilderTest.php
@@ -6,6 +6,9 @@ use Neoxygen\Neogen\Neogen;
 
 class BuilderTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @var \Neoxygen\Neogen\Neogen
+     */
     protected $neogen;
 
     public function setup()
@@ -19,6 +22,21 @@ class BuilderTest extends \PHPUnit_Framework_TestCase
     public function testParserManagerIsRegistered()
     {
         $this->assertNotEmpty($this->neogen->getParserManager());
+    }
+
+    public function testYamlParserIsAvailable()
+    {
+        $this->assertInstanceOf('Neoxygen\Neogen\Parser\YamlFileParser', $this->neogen->getParser('yaml'));
+    }
+
+    public function testCypherPaserIsAvailable()
+    {
+        $this->assertInstanceOf('Neoxygen\Neogen\Parser\CypherPattern', $this->neogen->getParser('cypher'));
+    }
+
+    public function testGraphGeneratorIsAvailable()
+    {
+        $this->assertInstanceOf('Neoxygen\Neogen\GraphGenerator\Generator', $this->neogen->getGraphGenerator());
     }
 }
 
